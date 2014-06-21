93960e50af5fef57631e22d0ca3218f7df55245b
GoblinHero <duckhero@gmail.com>
2014-06-21 22:08:48 +0200

    Learning plan and minor changes
    Læringsplan for introforløb

--- Gobbler/gobbler.py
+++ Gobbler/gobbler.py
@@ -87,7 +87,7 @@ class Enemy(pygame.sprite.Sprite):
         self.changeSpeed()
         self.image = grafik[1]
         self.rect = pygame.rect.Rect((450,240), self.image.get_size())
-        self.moving = 1 #Right
+        self.moving = 1 #1 = Højre, -1 = Venstre
     def changeSpeed(self):
         self.speed = random.randint(1,8)
     def update(self, dt, game):
